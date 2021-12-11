7ff408ae2fd4df22089d4dc5327c99798fbdbe91
Drew Templeton <templetonag@appstate.edu>
2021-12-11 18:27:11 -0500

    all functions complete

--- script.js
+++ script.js
@@ -1,52 +1,133 @@
-let display = '';
+let display = '0';
+let operator = '';
+let operandA = '-1';
+let operandB = '-1';
+let answer = '';
+let prefix = false;
 const output = document.querySelector('.output');
 
 //Calculator functions
-function add(x, y){
-    return x + y;
+function add(x, y) {
+    answer = x + y
+    output.textContent = answer;
 }
 
-function subtract(x, y){
-    return x - y;
+function subtract(x, y) {
+    answer = x - y
+    output.textContent = answer;
 }
 
-function mutiply(x, y){
-    return x * y;
+function multiply(x, y) {
+    answer = x * y
+    output.textContent = answer;
 }
 
-function divide(x, y){
-    if(y == 0)
-        return "ERROR"
-    return x / y;
+function divide(x, y) {
+    if (y == 0)
+        output.textContent = "ERROR"
+
+    else {
+        answer = x / y
+        output.textContent = answer;
+    }
 }
 
-function operate(x, y) {
+function mod(x, y) {
+    answer = x % y
+    output.textContent = answer;
+}
 
+function operate() {
+    let a = parseInt(operandA);
+    let b = parseInt(operandB);
+
+    switch (operator) {
+        case ('add'):
+            add(a, b);
+        break;
+
+        case ('subtract'):
+            subtract(a, b);
+        break;
+
+        case ('multiply'):
+            multiply(a, b);
+        break;
+
+        case ('divide'):
+            divide(a, b);
+        break;
+
+        case ('mod'):
+            mod(a, b);
+        break;
+    }
+
+    display = answer;
 }
 
 const numberBtn = document.querySelectorAll('.number');
 
-for(let i = 0; i < numberBtn.length; i++)
-{
+for (let i = 0; i < numberBtn.length; i++) {
     numberBtn[i].addEventListener('click', () => {
-            display += (numberBtn[i].getAttribute('data-num'))
-            displayNum();
+        display += (numberBtn[i].getAttribute('data-num'))
+        
+        if(prefix) {
+            display *= -1
+            prefix = false
+        }
+            
+        displayNum();
     })
-} 
- 
+}
+
 
 function displayNum() {
-    if(parseInt(display) == 0)
+    if (parseInt(display) == 0)
         display = 0;
 
-    else if(parseInt(display) > 0 && display[0] == '0')
+    else if (parseInt(display) > 0 && display[0] == '0')
         display = display.slice(1)
-        
+
     output.textContent = display;
 }
 
 const clearBtn = document.querySelector('.clear')
 clearBtn.addEventListener('click', () => {
-    display = ''
+    display = '0';
+    operator = '';
+    operandA = '';
+    operandB = '';
+    answer = '';
     output.textContent = 0;
+})
+
+
+const operatorBtn = document.querySelectorAll('.operator');
+
+for (let i = 0; i < operatorBtn.length; i++) {
+    operatorBtn[i].addEventListener('click', () => {
+        operator = (operatorBtn[i].getAttribute('id'))
+        if (answer)
+            operandA = display;
+
+        else
+            operandA = display;
+        display = '0';
+    })
+}
+
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', () => {
+    operandB = display;
+    operate()
+})
+
+const prefixBtn = document.querySelector('#prefix');
+prefixBtn.addEventListener('click', () => {
+    if(display != 0)
+        display *= -1
+    else
+        prefix = true;
+    displayNum()
 })
